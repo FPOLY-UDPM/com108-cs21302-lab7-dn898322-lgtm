36335923ebeb0792be18e3575263fdd61e470ee6
Nguyễn Tấn Dương <dn898322@gmail.com>
2026-02-28 10:02:59 +0700

    Add string sorting program in bai3.c
    Implemented a program to sort strings alphabetically using Bubble Sort.

--- bai3.c
+++ bai3.c
@@ -1,12 +1,45 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Nguyễn Tấn Dương]
+ * MSSV:      [ps47990]
+ * Lớp:       [cs21301]
  *****************************************************************************/
 
 //  BÀI 3: XÂY DỰNG CHƯƠNG TRÌNH SẮP XẾP CHUỖI THEO CHỮ CÁI 
 //  Input: Nhập 5 chuỗi bất kỳ 
 //  Output: Thứ tự các chuỗi đã được sắp xếp  
 
-// VIẾT CODE Ở ĐÂY
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[5][100];
+    char temp[100];
+
+    printf("Nhap 5 chuoi:\n");
+    for(int i = 0; i < 5; i++) {
+        printf("Chuoi %d: ", i + 1);
+        fgets(str[i], sizeof(str[i]), stdin);
+        str[i][strcspn(str[i], "\n")] = '\0'; // xoa ky tu xuong dong
+    }
+
+    // Sap xep theo thu tu alphabet (Bubble Sort)
+    for(int i = 0; i < 4; i++) {
+        for(int j = i + 1; j < 5; j++) {
+            if(strcmp(str[i], str[j]) > 0) {
+                strcpy(temp, str[i]);
+                strcpy(str[i], str[j]);
+                strcpy(str[j], temp);
+            }
+        }
+    }
+
+    printf("\nCac chuoi sau khi sap xep:\n");
+    for(int i = 0; i < 5; i++) {
+        printf("%s\n", str[i]);
+    }
+
+    return 0;
+}
+
 
