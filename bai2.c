283fbcb7d4658b42df80423e9cc16fe1a0f433b9
Nguyễn Tấn Dương <dn898322@gmail.com>
2026-02-28 10:02:19 +0700

    Add login program with username and password

--- bai2.c
+++ bai2.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Nguyễn Tấn Dương]
+ * MSSV:      [PS47990]
+ * Lớp:       [cs21301]
  *****************************************************************************/
 
 //  BÀI 2: XÂY DỰNG CHƯƠNG TRÌNH ĐĂNG NHẬP BẰNG USERNAME VÀ PASSWORD 
@@ -9,4 +9,40 @@
 //  Output: Đăng nhập thành công hay không thành công
 
 // VIẾT CODE Ở ĐÂY
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char username[50];
+    char password[50];
+
+    // Tài khoản mẫu (có thể thay đổi)
+    char correctUsername[] = "admin";
+    char correctPassword[] = "123456";
+
+    printf("Nhap username: ");
+    scanf("%s", username);
+
+    printf("Nhap password: ");
+    scanf("%s", password);
+
+    // So sánh username và password
+    if (strcmp(username, correctUsername) == 0 && 
+        strcmp(password, correctPassword) == 0) {
+        printf("Dang nhap thanh cong!\n");
+    } else {
+        printf("Dang nhap khong thanh cong!\n");
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
 
